901e6a589e4384480e49e549ac56f2c68f1bff78
sergfer26 <sfernandezm97@gmail.com>
2024-07-14 14:20:08 -0600

    _state -> state

--- env/env.py
+++ env/env.py
@@ -20,6 +20,8 @@ STEPS = ENV_PARAMS['STEPS']
 
 class QuadcopterEnv(gym.Env):
     def __init__(self, 
+                 check_contained: bool = False,
+                 is_training: bool = False, 
                  reward: callable = lambda x, u, i : -penalty(x, u, i),
                  low_obs: np.ndarray = LOW_OBS,
                  high_obs: np.ndarray = HIGH_OBS,
@@ -33,9 +35,26 @@ class QuadcopterEnv(gym.Env):
                                                 high=high_obs, 
                                                 dtype=np.float32)
         self.observation_space = copy(self._observation_space)
-        self._state = self.reset()  # estado interno del ambiente
+        self.state = self.reset()  # estado interno del ambiente
         self.set_time(STEPS, DT)
         self.reward = reward
+        self.check_contained = check_contained
+        self.is_training = is_training
+
+    def is_contained(self, state):
+        '''
+            Verifica si los valores de (x, y, z) estan dentro
+            de los rangos definidos por el ambiente.
+            Params:
+                * state (array): vector de 12 o 18 posiciones.
+            Salidas:
+                * (bool): estado de contención.
+        '''
+        x = state[3:6]
+        high = self._observation_space.high[3:6]
+        low = self._observation_space.low[3:6]
+        aux = np.logical_and(low <= x, x <= high)
+        return aux.all()
 
     def is_done(self):
         '''
@@ -43,10 +62,10 @@ class QuadcopterEnv(gym.Env):
 
             regresa valor booleano.
         '''
+        done = False
         if self.i == self.steps-1:  # Si se te acabo el tiempo
-            return True
-        else:
-            return False
+            done = True
+        return done
         
     def step(self, action):
         '''
@@ -73,13 +92,15 @@ class QuadcopterEnv(gym.Env):
         '''
         w1, w2, w3, w4 = action + W0
         t = [self.time[self.i], self.time[self.i+1]]
-        y_dot = odeint(f, self._state, t, args=(w1, w2, w3, w4))[1]  
+        y_dot = odeint(f, self.state, t, args=(w1, w2, w3, w4))[1]  
         # , Dfun=self.jac)[1]
-        self._state = y_dot
+        self.state = y_dot
         reward = self.reward(y_dot, action, self.i)
         done = self.is_done()
+        if self.check_contained:
+            done = self.is_contained(self._state)
         self.i += 1
-        return self._state, reward, done, None
+        return self.state, reward, done, None
     
     def set_time(self, steps, dt):
         '''
@@ -98,8 +119,8 @@ class QuadcopterEnv(gym.Env):
             regresa el estado actual del drone.
         '''
         self.i = 0
-        self._state = self._observation_space.sample()
-        return self._state    
+        self.state = self._observation_space.sample()
+        return self.state    
 
 
 class QuadcopterWrapper(gym.ObservationWrapper):
@@ -122,3 +143,41 @@ class QuadcopterWrapper(gym.ObservationWrapper):
     
     def reset(self, **kwargs):
         return super().reset(**kwargs)
+    
+
+# Ornstein-Ulhenbeck Process
+# Taken from #https://github.com/vitchyr/rlkit/blob/master/rlkit/exploration_strategies/ou_strategy.py
+class OUNoise(object):
+    def __init__(self, action_space, 
+                 mu=0.0,
+                 theta=0.15,
+                 max_sigma=0.9,
+                 min_sigma=0.05,
+                 decay_period=1e4
+                 ):
+        self.mu = mu
+        self.theta = theta
+        self.sigma = max_sigma
+        self.max_sigma = max_sigma
+        self.min_sigma = min_sigma
+        self.decay_period = decay_period
+        self.action_dim = action_space.shape[0]
+        self.low = action_space.low
+        self.high = action_space.high
+        self.reset()
+
+    def reset(self):
+        self.state = np.ones(self.action_dim) * self.mu
+
+    def evolve_state(self):
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * \
+            np.random.randn(self.action_dim)
+        self.state = x + dx
+        return np.clip(self.state, self.low, self.high)
+
+    def get_action(self, action, t=0):
+        ou_state = self.evolve_state()
+        self.sigma = self.max_sigma - \
+            (self.max_sigma - self.min_sigma) * min(1.0, t / self.decay_period)
+        return np.clip(action + ou_state, self.low, self.high)
