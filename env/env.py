51acd7e64516426ab3beff8dec87a38f182fb2ff
sergfer26 <sfernandezm97@gmail.com>
2025-10-09 13:11:55 -0600

    redifiend definition for is_contained

--- env/env.py
+++ env/env.py
@@ -105,12 +105,10 @@ class QuadcopterEnv(gym.Env):
             Whether the state is contained in the observation space.
         '''
         pos = state[3:6]
-        high = self.observation_space.high[3:6]
-        low = self.observation_space.low[3:6]
+        high = self.observation_space.high[3:6] + 2 
+        low = self.observation_space.low[3:6] - 2
         position_bool = np.logical_and(low <= pos, pos <= high)
-        angles = state[9:]
-        angles_bool = np.logical_and(-np.pi/2 <= angles, angles <= np.pi/2)
-        return position_bool.all() and angles_bool.all()
+        return position_bool.all()
 
     def is_done(self):
         '''
