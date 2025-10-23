1e641822fac549fe0c20e4485106062281064fb7
sergfer26 <sfernandezm97@gmail.com>
2025-10-22 21:34:09 -0600

    added __all__

--- env/__init__.py
+++ env/__init__.py
@@ -1,2 +1,4 @@
 from env.equations import f, angles2rotation, penalty, terminal_penalty
 from env.env import QuadcopterEnv, QuadcopterWrapper
+
+__all__ = ["f", "angles2rotation", "penalty", "terminal_penalty", "QuadcopterEnv", "QuadcopterWrapper"]
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
 from .report import create_report
 from .mail import send_resport, send_email
 
-__all__ = ["create_report", "send_email", "send_report"]
+__all__ = ["create_report", "send_email", "send_resport"]
